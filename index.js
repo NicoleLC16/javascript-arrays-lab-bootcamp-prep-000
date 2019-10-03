e7b6e8f1735481b490cc3c78e8d691c49106ac61
Nicole Cabral <asalwaysnicole@outlook.com>
2019-10-03 22:50:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ function prependKitten(name) {
   return (newKittens);
 }
 
-function removeLastKitten(name) {
-  return kittens.slice(0, slice.length - 1);
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1);
 }
 
 function removeFirstKitten(name) {
