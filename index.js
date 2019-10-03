3e02185f4d52e37aa6bfe041ecda8a471fb8ec8f
Nicole Cabral <asalwaysnicole@outlook.com>
2019-10-03 22:13:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"];
 
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return (kittens);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return (kittens);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop();
+  return (kittens);
+}
+
+function destructivelyRemoveFirstKitten (name) {
+  kittens.shift(name);
+  return (kittens);
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name];
+  return (newKittens);
+}
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens];
+  return (newKittens);
+}
+
+function removeLastKitten(name) {
+  
+  return kittens.slice(0,kittens.lenth - 1);
+}
